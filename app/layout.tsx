84a5890185d590bef350ef0786b416e7456d28e8
Pedro Almeida <pedroalmeida.415@gmail.com>
2024-05-18 18:32:11 -0300

    feat: add custom fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,23 @@
+import localFont from 'next/font/local'
 import { Layout } from '@/components/dom/Layout'
 import '@/global.css'
+
+const neue_montreal = localFont({
+  src: [
+    {
+      path: './fonts/neue-montreal/PPNeueMontreal-Light.woff2',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: './fonts/neue-montreal/PPNeueMontreal-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-neue-montreal',
+  fallback: ['sans-serif'],
+})
 
 export const metadata = {
   title: 'Next.js + Three.js',
@@ -8,7 +26,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en' className='antialiased'>
+    <html lang='en' className={`${neue_montreal.variable} antialiased`}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'], // remove unused styles in production
   darkMode: 'media', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'neue-montreal': ['var(--font-neue-montreal)'],
+      },
+    },
   },
   variants: {
     extend: {},
