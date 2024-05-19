da7dac8404b8e845b14eec82c7caef85e79a7e03
Pedro Almeida <pedroalmeida.415@gmail.com>
2024-05-19 18:19:14 -0300

    feat: add Showcase section

--- app/page.tsx
+++ app/page.tsx
@@ -22,6 +22,7 @@ const Common = dynamic(() => import('@/components/canvas/View').then((mod) => mo
 const Hero = dynamic(() => import('@/components/sections/Hero').then((mod) => mod.Hero), { ssr: false })
 const About = dynamic(() => import('@/components/sections/About').then((mod) => mod.About), { ssr: false })
 const Services = dynamic(() => import('@/components/sections/Services').then((mod) => mod.Services), { ssr: false })
+const Showcase = dynamic(() => import('@/components/sections/Showcase').then((mod) => mod.Showcase), { ssr: false })
 
 export default function Page() {
   return (
@@ -29,6 +30,7 @@ export default function Page() {
       <Hero />
       <About />
       <Services />
+      <Showcase />
     </>
   )
 }
---
+++ src/components/sections/Showcase.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import Image from 'next/image'
+
+import ShowcaseTitle from './assets/showcase.svg'
+import collamapImg from './assets/collamap.png'
+import rivellaImg from './assets/rivella.jpg'
+import freshland from './assets/freshland.png'
+import paretoImg from './assets/pareto.png'
+
+const Showcase = () => {
+  return (
+    <section className='overflow-hidden bg-[#1d1d1d] px-6 py-20'>
+      <h1 className='sr-only'>Showcase</h1>
+      <ShowcaseTitle
+        style={{
+          width: '177%',
+        }}
+        className='mb-32 h-auto'
+      />
+
+      <div className='flex'>
+        <div className='grid w-9/12 grid-flow-row grid-cols-2 gap-[6px]'>
+          <Image src={collamapImg} alt='Website Logo' />
+          <Image src={rivellaImg} alt='Website Logo' />
+          <Image src={freshland} alt='Website Logo' />
+          <Image src={paretoImg} alt='Website Logo' />
+        </div>
+        <div className='w-3/12 pl-6 text-white'>
+          <ul>
+            {Array.from({ length: 4 }).map((_, index) => (
+              <li key={index} className={`mb-9 ${index !== 0 ? 'opacity-20' : ''}`}>
+                <h3 className='mb-2 text-5xl uppercase'>Collamap</h3>
+                <p className='mb-4'>Front-end / Performance / Interaction</p>
+                <hr />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export { Showcase }
