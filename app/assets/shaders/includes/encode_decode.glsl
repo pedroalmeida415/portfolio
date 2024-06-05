6940face907dd40b1d8ca6b48ddbb26d88e11790
Pedro Almeida <pedroalmeida.415@gmail.com>
2024-06-05 01:02:07 -0300

    chore: encode/decode code cleanup

--- app/assets/shaders/includes/encode_decode.glsl
+++ app/assets/shaders/includes/encode_decode.glsl
@@ -1,5 +1,5 @@
-
 #define MAX_POS vec2(10.0, 5.0)
+#define POS_BIT_RANGE 0xfffff
 
 struct Particle {
     vec2 pos;
@@ -10,29 +10,36 @@ struct Particle {
 Particle decode_particle(vec4 particle_encoded) {
     Particle particle;
 
-    particle.pos.x = (float(abs(int(particle_encoded.x)) >> 1) / float(0xfffff)) * MAX_POS.x;
     float pos_x_sign = sign(particle_encoded.x);
+    uint iPos_x = uint(abs(particle_encoded.x));
+
+    uint iFlag_is_touched = iPos_x & uint(0x1);
+    particle.is_touched = bool(iFlag_is_touched);
+
+    iPos_x = iPos_x >> 1;
+    
+    particle.pos.x = float(iPos_x) / float(POS_BIT_RANGE);
+    particle.pos.x *= MAX_POS.x;
     particle.pos.x *= pos_x_sign;
     
-    particle.pos.y = (float(abs(int(particle_encoded.y)) >> 0) / float(0xfffff)) * MAX_POS.y;
+    int iPos_y = int(abs(particle_encoded.y));
+    particle.pos.y = (float(iPos_y >> 0) / float(POS_BIT_RANGE)) * MAX_POS.y;
     float pos_y_sign = sign(particle_encoded.y);
     particle.pos.y *= pos_y_sign;
 
-    particle.is_touched = bool(abs(int(particle_encoded.x)) & 0x1);
-    
     particle.delay = particle_encoded.a;
 
     return particle;
 }
 
 vec4 encode_particle(Particle particle) {
-    int iPos_x = int((min(abs(particle.pos.x), MAX_POS.x) / MAX_POS.x) * float(0xfffff));
-    float encoded_pos_x = float(((iPos_x & 0xfffff) << 1) | (int(particle.is_touched) << 0));
+    uint iPos_x = uint((min(abs(particle.pos.x), MAX_POS.x) / MAX_POS.x) * float(POS_BIT_RANGE));
+    iPos_x = iPos_x << 1 | uint(particle.is_touched);
     float pos_x_sign = sign(particle.pos.x);
-    encoded_pos_x *= pos_x_sign;
+    float encoded_pos_x = float(iPos_x) * pos_x_sign;
 
-    int iPos_y = int((min(abs(particle.pos.y), MAX_POS.y) / MAX_POS.y) * float(0xfffff));
-    float encoded_pos_y = float(((iPos_y & 0xfffff) << 0));
+    int iPos_y = int((min(abs(particle.pos.y), MAX_POS.y) / MAX_POS.y) * float(POS_BIT_RANGE));
+    float encoded_pos_y = float(((iPos_y & POS_BIT_RANGE) << 0));
     float pos_y_sign = sign(particle.pos.y);
     encoded_pos_y *= pos_y_sign;
 
