13259ae68aeb7050e12d1a5cc7589cd411fc8c87
Pedro Almeida <pedroalmeida.415@gmail.com>
2024-06-07 07:52:36 -0300

    feat: change particles mouse interaction

--- app/assets/shaders/gpgpu/particles.glsl
+++ app/assets/shaders/gpgpu/particles.glsl
@@ -6,60 +6,65 @@ uniform bool uIsLMBDown;
 
 @include "../includes/encode_decode.glsl"
 
-void main()
-{
+void main() {
     Particle particle;
     
     vec2 uv = gl_FragCoord.xy / resolution.xy;
     vec4 base = texture(uBase, uv);
     vec4 current = texture(uParticles, uv);
-
+    
     if (base == current) {
         particle.pos = vec2(0.0, -3.0);
         particle.delay = current.a;
         particle.is_touched = false;
-
+        
         gl_FragColor = encode_particle(particle);
         return;
     }
-
-    particle = decode_particle(current); 
+    
+    particle = decode_particle(current);
     
     float mouseDist = distance(uMouse, particle.pos);
-    float originDist = distance(base.xy, particle.pos);
-    float mouseOriginDist = distance(base.xy, uMouse);
-
-    if (mouseDist <= 1.0) {
-        particle.is_touched = true;
-    }
-
-    if (uIsLMBDown){
-        particle.is_touched = false;
-    }
-
-    if (particle.is_touched){
-        if(mouseDist <= 1.0) {
-            vec2 originDirection = normalize(uMouse - base.xy);
-
-            particle.pos += originDirection * 0.1 * (1.0 - mouseDist);
-        } else {
-            vec2 mouseDirection = normalize(particle.pos - uMouse);
-
-            particle.pos += mouseDirection * 0.1 * -mouseDist;
-        }
-    } else {
-        float easeFactor = 1.0 - pow(0.9, uDeltaTime * 60.0);
-
-        if (particle.delay <= 0.0){
-            particle.pos = mix(particle.pos, base.xy, easeFactor);
-        } else {
-            particle.delay -= mod(uDeltaTime, 1.0);
-        }
-    }
-
-    if (originDist < 0.001) {
-        particle.delay = base.a;
-    }
+    
+    vec2 direction = normalize(particle.pos - uMouse);
+    
+    // float force = 1.0 / mouseDist; // Linear decay
+    
+    // float force = 0.8 / pow(mouseDist, 1.4); // Inverse Square Law
+    
+    // float alpha = .5; // Decay rate
+    // float force = 1.0 * exp(-alpha * mouseDist); // Exponential decay force
+    
+    // float force = 1.0 / log(mouseDist + 1.0); // Logarithmic Force
+    
+    // float mu = 2.0; // Midpoint distance
+    // float alpha = -10.0; // Steepness
+    // float force = 1.0 / (1.0 + exp(-alpha * (mouseDist - mu))); // Sigmoid Force
+    
+    // float frequency = 10.0;
+    // float phase = uTime;
+    // float force = .2 * sin(mouseDist * frequency + phase); // Radial force
+    
+    // float threshold = 2.0;
+    // float force = (mouseDist < threshold) ? -1.0 / mouseDist : 1.0 / mouseDist; // Attraction/Repulsion Force with Threshold
+    
+    // float restLength = 2.0; // Rest length of the spring
+    // float force = -1.0 * (mouseDist - restLength); // Spring Force
+    
+    float force = 0.8 / mouseDist / (1.0 - exp(-mouseDist)); // Central Force
+    
+    // float attractionRadius = 1.0;
+    // float repulsionRadius = 2.0;
+    // float force = 1.0 * (exp(-mouseDist / attractionRadius) - exp(-mouseDist / repulsionRadius)); // Exponential Attraction/Repulsion
+    
+    // vec2 perpendicular = vec2(-direction.y, direction.x); // Perpendicular to the direction
+    // float force = 1.0 / mouseDist;
+    // vec2 displacement = perpendicular * force;
+    // particle.pos += displacement * 0.1; // Circular Attraction/Repulsion
+    
+    particle.pos += force * direction * 0.1;
+    
+    particle.pos += (base.xy - particle.pos) * 0.05;
     
     gl_FragColor = encode_particle(particle);
 }
