d980fbd3e022a7959d993bcc143fda55ea107098
Pedro Almeida <pedroalmeida.415@gmail.com>
2024-06-07 18:54:59 -0300

    feat: add particle delay on initial render

--- app/assets/shaders/gpgpu/particles.glsl
+++ app/assets/shaders/gpgpu/particles.glsl
@@ -14,7 +14,7 @@ void main() {
     vec4 current = texture(uParticles, uv);
     
     if (base == current) {
-        particle.pos = vec2(0.0, -3.0);
+        particle.pos = vec2(0.0, -5.0);
         particle.delay = current.a;
         particle.is_touched = false;
         
@@ -28,43 +28,15 @@ void main() {
     
     vec2 direction = normalize(particle.pos - uMouse);
     
-    // float force = 1.0 / mouseDist; // Linear decay
-    
-    // float force = 0.8 / pow(mouseDist, 1.4); // Inverse Square Law
-    
-    // float alpha = .5; // Decay rate
-    // float force = 1.0 * exp(-alpha * mouseDist); // Exponential decay force
-    
-    // float force = 1.0 / log(mouseDist + 1.0); // Logarithmic Force
-    
-    // float mu = 2.0; // Midpoint distance
-    // float alpha = -10.0; // Steepness
-    // float force = 1.0 / (1.0 + exp(-alpha * (mouseDist - mu))); // Sigmoid Force
-    
-    // float frequency = 10.0;
-    // float phase = uTime;
-    // float force = .2 * sin(mouseDist * frequency + phase); // Radial force
-    
-    // float threshold = 2.0;
-    // float force = (mouseDist < threshold) ? -1.0 / mouseDist : 1.0 / mouseDist; // Attraction/Repulsion Force with Threshold
-    
-    // float restLength = 2.0; // Rest length of the spring
-    // float force = -1.0 * (mouseDist - restLength); // Spring Force
-    
-    float force = 0.8 / mouseDist / (1.0 - exp(-mouseDist)); // Central Force
-    
-    // float attractionRadius = 1.0;
-    // float repulsionRadius = 2.0;
-    // float force = 1.0 * (exp(-mouseDist / attractionRadius) - exp(-mouseDist / repulsionRadius)); // Exponential Attraction/Repulsion
-    
-    // vec2 perpendicular = vec2(-direction.y, direction.x); // Perpendicular to the direction
-    // float force = 1.0 / mouseDist;
-    // vec2 displacement = perpendicular * force;
-    // particle.pos += displacement * 0.1; // Circular Attraction/Repulsion
+    float force = 0.7 / mouseDist / (1.0 - exp(-mouseDist)); // Central Force
     
     particle.pos += force * direction * 0.1;
     
-    particle.pos += (base.xy - particle.pos) * 0.05;
+    if (particle.delay <= 0.0) {
+        particle.pos += (base.xy - particle.pos) * 0.05;
+    } else {
+        particle.delay -= mod(uDeltaTime, 1.0);
+    }
     
     gl_FragColor = encode_particle(particle);
 }
