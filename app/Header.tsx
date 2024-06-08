9d971cbe52d5a70f8ea26c5fd5ba35639b4766e1
Pedro Almeida <pedroalmeida.415@gmail.com>
2024-06-07 23:26:03 -0300

    fix: header z-index

--- app/Header.tsx
+++ app/Header.tsx
@@ -3,7 +3,7 @@ import logo from '@/assets/logo.png'
 
 const Header = () => {
   return (
-    <header className='absolute left-0 top-0 z-10 w-full p-6'>
+    <header className='absolute left-0 top-0 w-full p-6'>
       <nav className='flex w-full justify-start text-sm uppercase'>
         <div className='mr-1 flex w-1/12 items-center'>
           <Image height={14} src={logo} alt='Website Logo' />
@@ -12,7 +12,7 @@ const Header = () => {
           <em className='mr-2 not-italic'>Available for new projects</em>
           <span className='size-[10px] rounded-full bg-lime-500'></span>
         </div>
-        <ul className='ml-auto flex w-2/12 items-center justify-between'>
+        <ul className='ml-auto flex w-2/12 items-center justify-between *:z-10'>
           <li>
             <a href='#about'>About</a>
           </li>
