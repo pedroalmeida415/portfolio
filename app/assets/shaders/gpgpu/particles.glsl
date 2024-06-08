af80d861f2c6272e4113b328d1e09bcf280a08f3
Pedro Almeida <pedroalmeida.415@gmail.com>
2024-06-07 22:37:01 -0300

    feat: initial particles start from center of letter

--- app/assets/shaders/gpgpu/particles.glsl
+++ app/assets/shaders/gpgpu/particles.glsl
@@ -14,7 +14,31 @@ void main() {
     vec4 current = texture(uParticles, uv);
     
     if (base == current) {
-        particle.pos = vec2(0.0, -5.0);
+        vec2 pos_mapped_origin = vec2(0.0, -5.0);
+        
+        float pX_threshold = -9.6043;
+        float eX_threshold = -5.1738;
+        float dX_threshold = -0.9457;
+        float roX_threshold = 3.3531;
+        
+        if (base.x > roX_threshold) {
+            vec2 ro_origin = vec2(7.2677, 1.3225);
+            pos_mapped_origin = ro_origin;
+        } else if (
+            base.x > dX_threshold) {
+            vec2 d_origin = vec2(1.1581, 1.3225);
+            pos_mapped_origin = d_origin;
+        } else if (
+            base.x > eX_threshold) {
+            vec2 e_origin = vec2(-3.0901, 1.3225);
+            pos_mapped_origin = e_origin;
+        } else if (
+            base.x > pX_threshold) {
+            vec2 p_origin = vec2(-7.3688, 1.3225);
+            pos_mapped_origin = p_origin;
+        }
+        
+        particle.pos = pos_mapped_origin;
         particle.delay = current.a;
         particle.is_touched = false;
         
