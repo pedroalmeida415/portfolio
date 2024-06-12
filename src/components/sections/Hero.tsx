25570955e925dae22a7e06ec360c7e773c59808a
Pedro Almeida <pedroalmeida.415@gmail.com>
2024-06-12 19:22:27 -0300

    refactor: gpgpu setup

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -42,11 +42,6 @@ const Hero = () => {
         <h1 className='sr-only'>Pedro Almeida</h1>
         <h2 className='mr-20 text-5xl font-extralight'>Creative Developer</h2>
       </div>
-      {/* <p className='absolute bottom-6 left-1/2 -translate-x-1/2 text-sm uppercase leading-none'>
-        Keep Scrolling
-        <DownArrow className='absolute -left-12 top-0 h-full w-auto' />
-        <DownArrow className='absolute -right-12 top-0 h-full w-auto' />
-      </p> */}
       <View
         onPointerDown={() => (isLMBDown = true)}
         onPointerUp={() => (isLMBDown = false)}
@@ -68,13 +63,15 @@ const SceneWrapper = () => {
 
   return (
     <Suspense fallback={null}>
-      {/* <Background renderTarget={backgroundRenderTarget} /> */}
+      <Background renderTarget={backgroundRenderTarget} />
       <Particles backgroundTexture={backgroundRenderTarget.texture} />
     </Suspense>
   )
 }
 
 const Particles = ({ backgroundTexture }: { backgroundTexture: THREE.Texture }) => {
+  const texture = useTexture('/pedro-rgb.png')
+
   const renderer = useThree((state) => state.gl)
   const viewport = useThree((state) => state.viewport)
   const pointer = useThree((state) => state.pointer)
@@ -82,185 +79,144 @@ const Particles = ({ backgroundTexture }: { backgroundTexture: THREE.Texture }) 
 
   const resolution = useMemo(() => renderer.getDrawingBufferSize(new THREE.Vector2()), [renderer])
 
+  const planeAreaRef = useRef<THREE.Mesh | null>()
+  const pointsRef = useRef<THREE.Points<THREE.BufferGeometry, THREE.ShaderMaterial> | null>()
+
   const visibleWidth = viewport.width
   const visibleHeight = viewport.height
 
-  // console.time('canvas')
-  // const canvast0 = performance.now()
-  const texture = useTexture('/pedro-rgb.png')
-  const textureHeight = (texture.image.height * visibleWidth) / texture.image.width
+  const { gpgpuCompute, baseGeometryCount, baseParticlesTexture, particlesVariable, particlesUvArray } = useMemo(() => {
+    const canvas = document.createElement('canvas')
+    const context = canvas.getContext('2d')
 
-  const canvas = document.createElement('canvas')
-  const context = canvas.getContext('2d')
+    canvas.width = texture.image.width
+    canvas.height = texture.image.height
 
-  canvas.width = texture.image.width
-  canvas.height = texture.image.height
+    // TODO draw image on offscreen canvas i.e. web worker
+    context.drawImage(texture.image, 0, 0, texture.image.width, texture.image.height)
 
-  // TODO draw image on offscreen canvas i.e. web worker
-  context.drawImage(texture.image, 0, 0, texture.image.width, texture.image.height, 0, 0, canvas.width, canvas.height)
+    // --- Get canvas data ---
+    const imageData = context.getImageData(0, 0, canvas.width, canvas.height)
+    const data = imageData.data // RGBA data (4 values per pixel)
 
-  // --- Get canvas data ---
-  const imageData = context.getImageData(0, 0, canvas.width, canvas.height)
-  const data = imageData.data // RGBA data (4 values per pixel)
+    // --- Create base geomerty ---
+    const positions = []
+    const order = []
 
-  // --- Create base geomerty ---
-  const baseGeometry = new THREE.BufferGeometry()
-  const positions = []
-  const order = []
+    const pixelGrouping = 2
 
-  const pixelGrouping = 2
+    for (let y = 0; y < canvas.height; y += pixelGrouping) {
+      if (canvas.height - y <= 24) break
 
-  for (let y = 0; y < canvas.height; y += pixelGrouping) {
-    if (canvas.height - y <= 24) break
+      for (let x = 0; x < canvas.width; x += pixelGrouping) {
+        let greenColorSum = 0
+        let count = 0
 
-    for (let x = 0; x < canvas.width; x += pixelGrouping) {
-      let greenColorSum = 0
-      let count = 0
+        for (let dy = 0; dy < pixelGrouping; dy++) {
+          for (let dx = 0; dx < pixelGrouping; dx++) {
+            const index = (x + dx + (y + dy) * canvas.width) * 4
+            if (data[index] !== 255) continue
+            greenColorSum += data[index + 1]
+            count++
+          }
+        }
 
-      for (let dy = 0; dy < pixelGrouping; dy++) {
-        for (let dx = 0; dx < pixelGrouping; dx++) {
-          const index = (x + dx + (y + dy) * canvas.width) * 4
-          if (data[index] !== 255) continue
-          greenColorSum += data[index + 1]
-          count++
+        if (count > 0) {
+          // Add point position and color
+          const textureAspect = texture.image.width / texture.image.height
+
+          const position = new THREE.Vector3(
+            ((x + pixelGrouping / 2) / canvas.width) * visibleWidth - visibleWidth / 2,
+            -((y + pixelGrouping / 2) / canvas.height) * (visibleWidth / textureAspect) +
+              visibleWidth / textureAspect / 2,
+            0,
+          )
+          positions.push(position.x, position.y, position.z)
+          order.push(greenColorSum / count / 255) // Normalize the color values
         }
       }
+    }
+    canvas.remove()
 
-      if (count > 0) {
-        // Add point position and color
-        const textureAspect = texture.image.width / texture.image.height
+    const baseGeometry = new THREE.BufferGeometry()
+    baseGeometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3))
+    baseGeometry.setAttribute('_order', new THREE.Float32BufferAttribute(order, 1))
 
-        const position = new THREE.Vector3(
-          ((x + pixelGrouping / 2) / canvas.width) * visibleWidth - visibleWidth / 2,
-          -((y + pixelGrouping / 2) / canvas.height) * (visibleWidth / textureAspect) +
-            visibleWidth / textureAspect / 2,
-          0,
-        )
-        positions.push(position.x, position.y, position.z)
-        order.push(greenColorSum / count / 255) // Normalize the color values
+    // --- Translate base geometry instead of points geometry for accurate raycast ---
+    const textureHeight = (texture.image.height * visibleWidth) / texture.image.width
+    baseGeometry.translate(0, -textureHeight / 2 + visibleHeight / 2, 0)
+
+    const orderAtt = baseGeometry.attributes._order
+    const totalStaggerDuration = 3.0
+
+    // --- GPU Compute ---
+    const baseGeometryCount = baseGeometry.attributes.position.count
+    const gpgpuSize = Math.ceil(Math.sqrt(baseGeometryCount))
+
+    const particlesUvArray = new Float32Array(baseGeometryCount * 2)
+
+    for (let y = 0; y < gpgpuSize; y++) {
+      for (let x = 0; x < gpgpuSize; x++) {
+        const i = y * gpgpuSize + x
+        const i2 = i * 2
+
+        // UV
+        const uvX = (x + 0.5) / gpgpuSize
+        const uvY = (y + 0.5) / gpgpuSize
+
+        particlesUvArray[i2 + 0] = uvX
+        particlesUvArray[i2 + 1] = uvY
       }
     }
-  }
 
-  baseGeometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3))
-  baseGeometry.setAttribute('_order', new THREE.Float32BufferAttribute(order, 1))
+    const gpgpuCompute = new GPUComputationRenderer(gpgpuSize, gpgpuSize, renderer)
 
-  // const canvast1 = performance.now()
-  // console.timeEnd('canvas')
-  // console.log(`Fetch texture + canvas parsing took ${canvast1 - canvast0} milliseconds.`)
+    // Texture to store particles position
+    const baseParticlesTexture = gpgpuCompute.createTexture()
 
-  // useEffect(() => {
-  //   const getBinary = async () => {
-  //     // console.time('fetch')
-  //     const fetcht0 = performance.now()
-  //     try {
-  //       const response = await fetch('/data.dat')
-  //       const data = new DataView(await response.arrayBuffer())
-  //       const tempArray = new Float32Array(data.byteLength / Float32Array.BYTES_PER_ELEMENT)
-  //       const len = tempArray.length
-  //       // Incoming data is raw floating point values
-  //       // with little-endian byte ordering.
-  //       for (let jj = 0; jj < len; ++jj) {
-  //         tempArray[jj] = data.getFloat32(jj * Float32Array.BYTES_PER_ELEMENT, true)
-  //       }
-  //       console.log(tempArray)
-  //     } catch (error) {
-  //       console.log(error)
-  //     } finally {
-  //       // console.timeEnd('fetch')
-  //       const fetcht1 = performance.now()
-  //       console.log(`Fetch and parsing took ${fetcht1 - fetcht0} milliseconds.`)
-  //     }
-  //   }
+    // Fill texture with particles values
+    for (let i = 0; i < baseGeometryCount; i++) {
+      const i3 = i * 3
+      const i4 = i * 4
 
-  //   getBinary()
-  // }, [])
-
-  // --- Translate base geometry instead of points geometry for accurate raycast ---
-  baseGeometry.translate(0, -textureHeight / 2 + visibleHeight / 2, 0)
-
-  const orderAtt = baseGeometry.attributes._order
-  const totalStaggerDuration = 3.0
-
-  // --- GPU Compute ---
-  const baseGeometryCount = baseGeometry.attributes.position.count
-  const gpgpuSize = Math.ceil(Math.sqrt(baseGeometryCount))
-
-  const gpgpuCompute = useMemo(() => new GPUComputationRenderer(gpgpuSize, gpgpuSize, renderer), [gpgpuSize, renderer])
-
-  // Texture to store particles position
-  const baseParticlesTexture = gpgpuCompute.createTexture()
-
-  // Fill texture with particles values
-  for (let i = 0; i < baseGeometryCount; i++) {
-    const i3 = i * 3
-    const i4 = i * 4
-
-    // RGBA values for FBO texture from base geometry position
-    baseParticlesTexture.image.data[i4 + 0] = baseGeometry.attributes.position.array[i3 + 0]
-    baseParticlesTexture.image.data[i4 + 1] = baseGeometry.attributes.position.array[i3 + 1]
-    baseParticlesTexture.image.data[i4 + 2] = baseGeometry.attributes.position.array[i3 + 2]
-    baseParticlesTexture.image.data[i4 + 3] = totalStaggerDuration * orderAtt.array[i]
-  }
-
-  // Particles variable
-  const particlesVariable = gpgpuCompute.addVariable('uParticles', gpgpuParticlesShader, baseParticlesTexture)
-  gpgpuCompute.setVariableDependencies(particlesVariable, [particlesVariable])
-
-  // Uniforms
-  particlesVariable.material.uniforms.uIsLMBDown = new THREE.Uniform(isLMBDown)
-  particlesVariable.material.uniforms.uMouse = new THREE.Uniform(new THREE.Vector2(0, 100))
-  particlesVariable.material.uniforms.uTime = new THREE.Uniform(0)
-  particlesVariable.material.uniforms.uDeltaTime = new THREE.Uniform(0)
-  particlesVariable.material.uniforms.uBase = new THREE.Uniform(baseParticlesTexture)
-
-  // Init
-  gpgpuCompute.init()
-
-  // Geometry
-  const particlesUvArray = new Float32Array(baseGeometryCount * 2)
-
-  for (let y = 0; y < gpgpuSize; y++) {
-    for (let x = 0; x < gpgpuSize; x++) {
-      const i = y * gpgpuSize + x
-      const i2 = i * 2
-
-      // UV
-      const uvX = (x + 0.5) / gpgpuSize
-      const uvY = (y + 0.5) / gpgpuSize
-
-      particlesUvArray[i2 + 0] = uvX
-      particlesUvArray[i2 + 1] = uvY
+      // RGBA values for FBO texture from base geometry position
+      baseParticlesTexture.image.data[i4 + 0] = baseGeometry.attributes.position.array[i3 + 0]
+      baseParticlesTexture.image.data[i4 + 1] = baseGeometry.attributes.position.array[i3 + 1]
+      baseParticlesTexture.image.data[i4 + 2] = baseGeometry.attributes.position.array[i3 + 2]
+      baseParticlesTexture.image.data[i4 + 3] = totalStaggerDuration * orderAtt.array[i]
     }
-  }
+    baseGeometry.dispose()
 
-  const particlesGeometry = new THREE.BufferGeometry()
-  particlesGeometry.setDrawRange(0, baseGeometryCount)
-  particlesGeometry.setAttribute('aParticlesUv', new THREE.BufferAttribute(particlesUvArray, 2))
+    // Particles variable
+    const particlesVariable = gpgpuCompute.addVariable('uParticles', gpgpuParticlesShader, baseParticlesTexture)
+    gpgpuCompute.setVariableDependencies(particlesVariable, [particlesVariable])
 
-  // ---  Particle shader  ---
-  const material = new THREE.ShaderMaterial({
-    vertexShader: particlesVertexShader,
-    fragmentShader: particlesFragmentShader,
-    uniforms: {
-      uSize: new THREE.Uniform(0.035),
-      uResolution: new THREE.Uniform(new THREE.Vector2(resolution.x, resolution.y)),
-      uParticlesTexture: new THREE.Uniform(null),
-      uBaseParticlesTexture: new THREE.Uniform(baseParticlesTexture),
-      uBackgroundTexture: new THREE.Uniform(backgroundTexture),
-    },
-    depthTest: false,
-    transparent: true,
-  })
+    // Uniforms
+    particlesVariable.material.uniforms.uIsLMBDown = { value: isLMBDown }
+    particlesVariable.material.uniforms.uMouse = { value: new THREE.Vector2(0, -100) }
+    particlesVariable.material.uniforms.uTime = { value: 0 }
+    particlesVariable.material.uniforms.uDeltaTime = { value: 0 }
+    particlesVariable.material.uniforms.uBase = { value: baseParticlesTexture }
 
-  const points = new THREE.Points(particlesGeometry, material)
-  const planeArea = useRef<THREE.Mesh | null>()
+    // Init
+    gpgpuCompute.init()
+
+    return {
+      gpgpuCompute,
+      baseGeometryCount,
+      baseParticlesTexture,
+      particlesVariable,
+      particlesUvArray,
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  useEffect(() => () => gpgpuCompute.dispose(), [gpgpuCompute])
 
   const uMouseVec = new THREE.Vector2()
-
   useFrame((state, delta) => {
     state.raycaster.setFromCamera(state.pointer, state.camera)
-    const intersects = state.raycaster.intersectObject(planeArea.current)
+    const intersects = state.raycaster.intersectObject(planeAreaRef.current)
 
     if (intersects.length > 0) {
       particlesVariable.material.uniforms.uMouse.value = uMouseVec.set(intersects[0].point.x, intersects[0].point.y)
@@ -271,17 +227,33 @@ const Particles = ({ backgroundTexture }: { backgroundTexture: THREE.Texture }) 
     particlesVariable.material.uniforms.uDeltaTime.value = delta
     particlesVariable.material.uniforms.uIsLMBDown.value = isLMBDown
     gpgpuCompute.compute()
-    material.uniforms.uParticlesTexture.value = gpgpuCompute.getCurrentRenderTarget(particlesVariable).texture
+    pointsRef.current.material.uniforms.uParticlesTexture.value =
+      gpgpuCompute.getCurrentRenderTarget(particlesVariable).texture
   })
-
-  useEffect(() => {
-    return () => gpgpuCompute.dispose()
-  }, [gpgpuCompute])
 
   return (
     <>
-      <primitive object={points} position={[0, 0, 0.001]} />
-      <mesh ref={planeArea}>
+      <points ref={pointsRef} position={[0, 0, 0.001]}>
+        <bufferGeometry ref={(ref) => ref?.setDrawRange(0, baseGeometryCount)}>
+          <bufferAttribute attach='attributes-aParticlesUv' array={particlesUvArray} itemSize={2} />
+        </bufferGeometry>
+        <shaderMaterial
+          transparent
+          depthTest={false}
+          vertexShader={particlesVertexShader}
+          fragmentShader={particlesFragmentShader}
+          uniforms={{
+            uSize: { value: 0.035 },
+            uResolution: { value: null },
+            uParticlesTexture: { value: null },
+            uBaseParticlesTexture: { value: baseParticlesTexture },
+            uBackgroundTexture: { value: backgroundTexture },
+          }}
+        >
+          <vector2 attach='uniforms-uResolution-value' args={[resolution.x, resolution.y]} />
+        </shaderMaterial>
+      </points>
+      <mesh ref={planeAreaRef}>
         <planeGeometry args={[visibleWidth, visibleHeight]} />
         <meshBasicMaterial map={backgroundTexture} transparent />
       </mesh>
@@ -303,14 +275,24 @@ const Background = ({ renderTarget }: { renderTarget: THREE.WebGLRenderTarget<TH
       materialRef.current.uniforms.uTime.value = state.clock.elapsedTime
     }
 
+    const currentXrEnabled = renderer.xr.enabled
+    const currentShadowAutoUpdate = renderer.shadowMap.autoUpdate
+
+    renderer.xr.enabled = false // Avoid camera modification
+    renderer.shadowMap.autoUpdate = false // Avoid re-computing shadows
+
     state.gl.setRenderTarget(renderTarget)
     state.gl.render(scene, cam)
+
+    renderer.xr.enabled = currentXrEnabled
+    renderer.shadowMap.autoUpdate = currentShadowAutoUpdate
+
     state.gl.setRenderTarget(null)
   })
 
   return createPortal(
     <mesh frustumCulled={false}>
-      <bufferGeometry drawRange={{ start: 0, count: 3 }} />
+      <bufferGeometry ref={(ref) => ref?.setDrawRange(0, 3)} />
       <rawShaderMaterial
         ref={materialRef}
         glslVersion={THREE.GLSL3}
@@ -319,11 +301,13 @@ const Background = ({ renderTarget }: { renderTarget: THREE.WebGLRenderTarget<TH
         transparent
         depthTest={false}
         uniforms={{
-          uTime: new THREE.Uniform(0),
-          uSeed: new THREE.Uniform(Math.random() * 100),
-          uResolution: new THREE.Uniform(new THREE.Vector2(resolution.x, resolution.y)),
+          uTime: { value: 0 },
+          uSeed: { value: Math.random() * 100 },
+          uResolution: { value: null },
         }}
-      />
+      >
+        <vector2 attach='uniforms-uResolution-value' args={[resolution.x, resolution.y]} />
+      </rawShaderMaterial>
     </mesh>,
     scene,
   )
