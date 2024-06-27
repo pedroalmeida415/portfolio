4847a6a8195c17f3149868114a0d34fcc31aae1a
Pedro Almeida <pedroalmeida.415@gmail.com>
2024-06-27 07:53:04 -0300

    refactor: get-binary hook to have better return structure

--- src/helpers/use-get-binary.ts
+++ src/helpers/use-get-binary.ts
@@ -2,41 +2,48 @@ import { suspend } from 'suspend-react'
 
 import { LZMA } from '@/helpers/lzma'
 
-async function getBinaries() {
-  const files = ['/pedro-positions.lzma', '/pedro-stagger-multipliers.lzma']
+const files = [
+  {
+    name: 'multipliers',
+  },
+  {
+    name: 'positions',
+    type: Float32Array,
+  },
+]
 
+async function getBinaries() {
   const responses = await Promise.all(
-    files.map((path) =>
+    files.map((file) =>
       fetch(
-        new Request(path, {
+        new Request(`/${file.name}.bin`, {
           method: 'GET',
         }),
-      ),
+      )
+        .then((response) => response.arrayBuffer())
+        .then((buffer) => {
+          const decompressedStreamBuffer = LZMA.decompressFile(buffer)
+          const rawBytes: Uint8Array = decompressedStreamBuffer.toUint8Array()
+
+          return {
+            fileName: file.name,
+            data: file.type ? new file.type(rawBytes.buffer) : rawBytes,
+          }
+        }),
     ),
   )
 
-  const arrAggregated = []
-
-  for (const response of responses) {
-    if (!response.ok) {
-      // This will activate the closest `error.js` Error Boundary
-      throw new Error('Failed to fetch data')
-    }
-    const buffer = await response.arrayBuffer()
-
-    const outStream = await LZMA.decompressFile(buffer)
-    const bytes: Uint8Array = outStream.toUint8Array()
-
-    const typedArray = response.url.includes('positions') ? new Float32Array(bytes.buffer) : bytes
-
-    arrAggregated.push(typedArray)
-  }
-
-  return arrAggregated
+  return responses.reduce(
+    (acc, response) => {
+      acc[response.fileName] = response.data
+      return acc
+    },
+    {} as Record<string, any>,
+  )
 }
 
 export const useGetBinary = () => {
-  const [positions, staggerMultipliers] = suspend(getBinaries, [])
+  const { positions, multipliers } = suspend(getBinaries, [])
 
-  return [positions, staggerMultipliers]
+  return [positions, multipliers]
 }
