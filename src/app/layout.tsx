208b15a1165e11997df3fb2f0054bd7e30b37918
Pedro Almeida <pedroalmeida.415@gmail.com>
2024-06-28 03:46:58 -0300

    fix: metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import localFont from 'next/font/local'
 import { Layout } from '@/components/dom/Layout'
 import { Navbar } from '@/components/navbar/navbar'
 import './global.css'
+import { Metadata, Viewport } from 'next'
 
 const neue_montreal = localFont({
   src: '../assets/fonts/neue-montreal/PPNeueMontreal-Variable.woff2',
@@ -16,14 +17,89 @@ const neue_montreal = localFont({
   ],
 })
 
+const title = 'React Three Next Starter'
+const description = 'The easiest and fastest way to create a 3D website using React Three Fiber and NextJS'
+
+// either Static metadata
+export const metadata: Metadata = {
+  title,
+  description,
+  keywords: 'Software Engineer,Product Manager,Project Manager,Data Scientist,Computer Scientist',
+  robots: 'index,follow',
+  authors: {
+    name: 'Author',
+  },
+  twitter: {
+    creator: '@pmndrs',
+    card: 'summary',
+    site: '@pmndrs',
+  },
+  openGraph: {
+    title,
+    siteName: title,
+    description,
+    type: 'website',
+    url: 'https://react-three-next.vercel.app/',
+    images: [
+      {
+        url: '/icons/share.png',
+        width: 1200,
+        height: 630,
+        alt: 'Share Image',
+      },
+    ],
+  },
+  icons: [
+    {
+      type: 'shortcut icon',
+      url: '/icons/apple-touch-icon.png',
+    },
+    {
+      sizes: '16x16',
+      url: '/icons/favicon-16x16.png',
+      type: 'apple-touch-icon',
+    },
+    {
+      sizes: '32x32',
+      url: '/icons/favicon-32x32.png',
+      type: 'apple-touch-icon',
+    },
+    {
+      sizes: '192x192',
+      url: '/icons/apple-touch-icon.png',
+      type: 'apple-touch-icon',
+    },
+    {
+      type: 'mask-icon',
+      color: '#000000',
+      url: '/icons/safari-pinned-tab.svg',
+    },
+    {
+      type: 'apple-touch-icon',
+      url: '/icons/apple-touch-icon.png',
+    },
+  ],
+  manifest: '/manifest.json',
+  formatDetection: {
+    telephone: true,
+  },
+  other: {
+    language: 'english',
+    distribution: 'web',
+    HandheldFriendly: 'true',
+  },
+}
+export const viewport: Viewport = {
+  minimumScale: 1,
+  initialScale: 1.0,
+  themeColor: 'black',
+  width: 'device-width',
+  userScalable: false,
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang='en' className={`${neue_montreal.variable}`}>
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
       <body>
         <Navbar />
         <Layout>{children}</Layout>
