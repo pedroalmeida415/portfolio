cb8b548e8143b0ae964508e4c6be367cd90d83cf
Pedro Almeida <pedroalmeida.415@gmail.com>
2024-07-23 08:28:46 -0300

    feat: responsive font-size

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -44,7 +44,7 @@ export const Home = () => {
   return (
     <section className='relative flex h-screen w-full items-end justify-start p-6'>
       <h1 className='absolute left-6 top-6 leading-none'>Pedro Almeida</h1>
-      <ul className='absolute left-[65.5rem] top-6 flex items-center justify-start gap-x-10 leading-none'>
+      <ul className='absolute right-[16.5rem] top-6 flex items-center justify-start gap-x-10 leading-none'>
         {socials.map(({ link, label }) => (
           <li key={label}>
             <a className='underline-offset-4 hover:underline' href={link} target='_blank' rel='noopener noreferrer'>
@@ -62,7 +62,7 @@ export const Home = () => {
           <h3>
             <strong className='mr-2 font-normal tracking-wide'>Available for new projects</strong>
           </h3>
-          <span id='ping' className='size-[10px] rounded-full bg-lime-500'></span>
+          <span id='ping' className='size-2.5 rounded-full bg-lime-500'></span>
         </div>
       </div>
       <nav className='absolute bottom-6 left-1/2 -translate-x-1/2 rounded-full p-2'>
@@ -70,15 +70,12 @@ export const Home = () => {
           {routes.map(({ path, label, icon: Icon }) => (
             <li key={path}>
               <a href={path} className='block rounded-full px-6 py-2 underline-offset-4 hover:underline'>
-                {Icon ? <Icon /> : label}
+                {Icon ? <Icon className='size-[1.125rem]' /> : label}
               </a>
             </li>
           ))}
         </ul>
       </nav>
-      <div id='progress-bar' className='progress completed'>
-        <div className='progress-value completed'></div>
-      </div>
     </section>
   )
 }
