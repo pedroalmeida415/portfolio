0ef762aec34ca8d592a92e4b34665268d6b49faa
Pedro Almeida <pedroalmeida.415@gmail.com>
2024-08-03 21:17:47 -0300

    fix: particles data fetch side effect

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -10,17 +10,20 @@ import { Canvas } from '~/components/canvas/canvas'
 
 import { LZMA } from '~/helpers/lzma'
 
+let didInit = false
+
 export const Layout = ({ children }: PropsWithChildren) => {
   const [particlesData, setParticlesData] = useAtom(particlesDataAtom)
 
   const eventSourceRef = useRef<HTMLElement | null>(null)
 
   useEffect(() => {
-    const controller = new AbortController()
+    if (didInit) return
+
     async function fetchParticlesData() {
       try {
-        const positionsPromise = getParticlesData('positions', controller.signal)
-        const multipliersPromise = getParticlesData('multipliers', controller.signal)
+        const positionsPromise = getParticlesData('positions')
+        const multipliersPromise = getParticlesData('multipliers')
         const [positions, multipliers] = await Promise.all([positionsPromise, multipliersPromise])
 
         setParticlesData({ positions: new Float32Array(positions.buffer), multipliers })
@@ -29,10 +32,7 @@ export const Layout = ({ children }: PropsWithChildren) => {
       }
     }
     fetchParticlesData()
-
-    return () => {
-      controller.abort('Fetching particles data was aborted')
-    }
+    didInit = true
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
@@ -51,8 +51,8 @@ export const Layout = ({ children }: PropsWithChildren) => {
   )
 }
 
-async function getParticlesData(type: 'positions' | 'multipliers', signal: AbortSignal) {
-  const res = await fetch(`/${type}.bin`, { signal })
+async function getParticlesData(type: 'positions' | 'multipliers') {
+  const res = await fetch(`/${type}.bin`)
   const buffer = await res.arrayBuffer()
   const decompressedStreamBuffer = LZMA.decompressFile(buffer)
   const rawBytes: Uint8Array = decompressedStreamBuffer.toUint8Array()
